e12386a5bc075db7aa54c2d3c5d89ae878fe8d1b
Isaac <newdevcoding@gmail.com>
2024-12-29 15:46:43 -0500

    created basic search form and form reset compoents and added search form to UI

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,22 @@
 import Image from "next/image";
+import SearchForm from "@/app/components/SearchForm";
 
 
 export default function Home() {
   return (
     <> 
-      <h1 className="heading">HOME</h1>
+      <section className="pink_container">
+        <h1 className="heading">Pitch Your Startup, <br /> Connect With Entrapreneurs</h1>
+
+        <p className="sub-heading !max-w-3xl">
+          Submit Ideas, Vote on Pitches, and Bring Your Idea to Life
+        </p>
+
+        <SearchForm />
+      
+      </section>
+
+      
     </>
   );
 }
