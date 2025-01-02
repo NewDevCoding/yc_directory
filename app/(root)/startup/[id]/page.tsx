6f5706e35a9b5059a9643184b640a3c4f6082baa
Isaac <newdevcoding@gmail.com>
2025-01-02 09:50:16 -0500

    created query to fetch views and finished ping and view components to display them
    NOTE: something about the view fetching breaks the app at this point

--- app/(root)/startup/[id]/page.tsx
+++ app/(root)/startup/[id]/page.tsx
@@ -26,19 +26,19 @@ const page = async ({ params }: { params: Promise<{id: string}>}) => {
   
     return (
     <>
-        <section className='pink_container !min-h-[230px]'>
-            <p className='tag'>{formatDate(post?._createdAt)}</p>
+        <section className="pink_container !min-h-[230px]">
+            <p className="tag">{formatDate(post?._createdAt)}</p>
 
-            <h1 className='heading'>{post.title}</h1>
-            <p className='sub-heading !max-w-5xl'>{post.description}</p>        
+            <h1 className="heading">{post.title}</h1>
+            <p className="sub-heading !max-w-5xl">{post.description}</p>
         </section>
 
-        <section className='section_container'>
+        <section className="section_container">
             <img
-            src={post.image}
-            alt="thumbnail"
-            className="w-full h-auto rounded-xl"
-            />
+              src={post.image}
+              alt="thumbnail"
+              className="w-full h-auto rounded-xl"
+             />
 
             <div className='space-y-5 mt-10 max-w-4xl mx-auto'>
                 <div className='flex-between gap-5'>
--- components/View.tsx
+++ components/View.tsx
@@ -1,14 +1,35 @@
-import React from 'react'
-import Ping from './Ping'
+import Ping from "@/components/Ping";
+import { client } from "@/sanity/lib/client";
+import { STARTUP_VIEWS_QUERY } from "@/sanity/lib/queries";
+import { writeClient } from "@/sanity/lib/write-client";
 
-const View = ({ id }: { id: string }) => {
+const View = async ({ id }: { id: string }) => {
+
+  const formatViews = (views: any) => {
+    if(views = 1){
+      return "1 View"
+    } else {
+      return `${views} Views`
+    }
+  }
+
+  const { views: totalViews } = await client
+  .withConfig({ useCdn: false })
+  .fetch(STARTUP_VIEWS_QUERY, { id });
+
+  await writeClient
+    .patch(id)
+    .set({ views: totalViews + 1 })
+    .commit()
+
+
   return (
     <div className='view-container'>
         <div className='absolute -top-2 -right-2'>
             <Ping />
         </div>
         <p className='view-text'>
-            <span className='font-black'>100 views</span>
+            <span className='font-black'>views</span>
         </p>
     </div>
   )
