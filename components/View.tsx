9e1dc1ff4026798c76cdc4ab5050ed2be324297e
Isaac <newdevcoding@gmail.com>
2025-01-02 10:14:47 -0500

    enabled after function in view to avoid blocking content load as views are fetched, enabled after in nect.config, changed useCdn to true in client.ts

--- components/View.tsx
+++ components/View.tsx
@@ -2,6 +2,7 @@ import Ping from "@/components/Ping";
 import { client } from "@/sanity/lib/client";
 import { STARTUP_VIEWS_QUERY } from "@/sanity/lib/queries";
 import { writeClient } from "@/sanity/lib/write-client";
+import {after} from "next/server";
 
 
 const View = async ({ id }: { id: string }) => {
@@ -18,10 +19,14 @@ const View = async ({ id }: { id: string }) => {
           }
         }
   
-  await writeClient
-    .patch(id)
-    .set({ views: totalViews + 1 })
-    .commit()
+  after(
+    async () => {
+      await writeClient
+      .patch(id)
+      .set({ views: totalViews + 1 })
+      .commit()
+    }
+  )
   
 
   return (
