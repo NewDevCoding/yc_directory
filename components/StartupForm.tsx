b7841dfdf8d53a72c9328a0835b2094ed6b0adfe
Isaac <newdevcoding@gmail.com>
2025-01-02 19:46:02 -0500

    created validation ts

--- components/StartupForm.tsx
+++ components/StartupForm.tsx
@@ -1,15 +1,50 @@
 "use client"
 
-import React, { useState } from 'react'
+import React, { useActionState, useState } from 'react'
 import { Input } from './ui/input'
 import { Textarea } from './ui/textarea';
 import MDEditor from '@uiw/react-md-editor';
+import { Button } from './ui/button';
+import { Send } from 'lucide-react';
+import { formSchema } from '@/lib/validation';
 
 const StartupForm = () => {
     
     const [errors, setErrors] = useState<Record<string, string>>({});
 
-    const [pitch, setPitch] = useState("Hello World")
+    const [pitch, setPitch] = useState("")
+
+    const handleFormSubmit = async (prevState: any, formData: FormData) => {
+        try {
+            const formValues = {
+                title: formData.get('title') as string,
+                description: formData.get('description') as string,
+                category: formData.get('category') as string,
+                link: formData.get('link') as string,
+                pitch,
+            }
+
+            await formSchema.parseAsync(formValues)
+
+            console.log(formValues)
+
+            // const result = await createDiffieHellman(prevState, formData, pitch)
+
+            // console.log(result)
+
+        } catch (error) {
+            
+        } finally {
+
+        }
+    }
+
+    const [state, formAction, isPending] = useActionState(handleFormSubmit,
+        { 
+            error: "",
+            status: "INITIAL"
+        })
+
   return (
     <form action={() => {}} className='startup-form'>
         <div>
@@ -67,10 +102,30 @@ const StartupForm = () => {
             <MDEditor
                 value={pitch}
                 onChange={(value) => setPitch(value as string)}
+                id='pitch'
+                preview='edit'
+                height={300}
+                style={{borderRadius: 20, overflow: 'hidden'}}
+                textareaProps={{
+                    placeholder: 'Describe your idea and explain why other users should upvote it'
+                }}
+                previewOptions={{
+                    disallowedElements: ['style']
+                }}
+            
             />
 
             {errors.pitch && <p className='startup-form_error'>{errors.pitch}</p>}
         </div>
+
+        <Button 
+            type='submit'
+            className='startup-form_btn text-white'
+            disabled={isPending}
+        >
+            {isPending ? 'Submitting...' : 'Submit'}
+            <Send  className="size-6 ml-2"/>
+        </Button>
         
     </form>
   )
